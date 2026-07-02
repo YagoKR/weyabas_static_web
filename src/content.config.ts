6e781e41230016cb27599e557f940273bae92275
Carlos A. L. <icarlush@protonmail.com>
2026-07-02 16:33:39 +0200

    Creacion de blog.astro (/blog). Prueba: agregar titulos de articulos dinamicamente a la pagina.

---
+++ src/content.config.ts
@@ -0,0 +1,21 @@
+import {defineCollection, z} from "astro:content";
+import { glob } from "astro/loaders";
+
+const postsCollection = defineCollection ({
+    loader: glob({
+        pattern: "**/*.{md,mdx}",
+        base: "./src/content/posts"
+    }),
+    // Aqui se definen los "metadatos" del post. El tipo de dato es configurable por nosotros dependiendo de lo que queramos.
+    schema: z.object({
+        author: z.string(),
+        date: z.string(),
+        title: z.string(),
+        categories: z.array(z.string())
+    })
+})
+
+// Se exporta la coleccion con la palabra clave 'posts'
+export const collections = {
+    posts: postsCollection
+}
---
+++ src/content/posts/test.md
@@ -0,0 +1,23 @@
+---
+author: Carlos D. Angel
+categories: ["test post"]
+date: 02/07/2026
+title: Test Post
+---
+
+This is a sample post. This text is not particularly formatted in any way.
+
+# This is an H1. How exciting.
+## And now an H2. How daring.
+
+This text is **bold**. This text is *italicized*. This text is... `code`. I guess. How will it render? I don't know.
+
+> Nothing at all.
+A wise man once said.
+
+We'll even try a table here in a moment.
+
+| Column1 | Column2           |
+|---------|-------------------|
+| Woah    | Columns and rows! |
+
