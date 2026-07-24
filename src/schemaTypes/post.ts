3066e6f97e0acd6eda5f1b635a68d012d6cc96e8
Yago <yago4088@gmail.com>
2026-07-24 16:37:18 +0200

    Cambio minimo

--- src/schemaTypes/post.ts
+++ src/schemaTypes/post.ts
@@ -1,34 +1,34 @@
 // src/schemaTypes/post.ts
 export default {
-  name: 'posts',
-  title: 'Posts',
-  type: 'document',
-  fields: [
-    {
-      name: 'title',
-      title: 'Título',
-      type: 'string',
-    },
-    {
-      name: 'author',
-      title: 'Autor',
-      type: 'string',
-    },
-    {
-      name: 'date',
-      title: 'Fecha',
-      type: 'datetime',
-    },
-    {
-      name: 'categories',
-      title: 'Categorías',
-      type: 'array',
-      of: [{ type: 'string' }],
-    },
-    {
-      name: 'summary',
-      title: 'Resumen',
-      type: 'text',
-    },
-  ],
+    name: 'posts',
+    title: 'Posts',
+    type: 'document',
+    fields: [
+        {
+            name: 'title',
+            title: 'Título',
+            type: 'string',
+        },
+        {
+            name: 'author',
+            title: 'Autor',
+            type: 'string',
+        },
+        {
+            name: 'date',
+            title: 'Fecha',
+            type: 'datetime',
+        },
+        {
+            name: 'categories',
+            title: 'Categorías',
+            type: 'array',
+            of: [{ type: 'string' }],
+        },
+        {
+            name: 'summary',
+            title: 'Resumen',
+            type: 'text',
+        },
+    ],
 };
