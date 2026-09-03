961f77cd5fb769f8c3ef97a226eef3f2d7208393
Yago <yago4088@gmail.com>
2026-09-03 23:31:53 +0200

    Añadido un script para ejecutar Google Analytics. Insatalada dependencia de partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import node from '@astrojs/node';
 
+import partytown from '@astrojs/partytown';
+
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: 'standalone',
   }),
-  integrations: [react()],
+  integrations: [react(), partytown()],
 });
